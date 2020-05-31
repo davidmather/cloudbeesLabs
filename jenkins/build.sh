96d07df34ce50fc77b54f0f6b15d82635fdaebe9
David <davidmather@live.ie>
2020-05-31 23:50:08 +0100

    add java file

--- jenkins/build.sh
+++ jenkins/build.sh
@@ -1,3 +1,6 @@
-echo "test"
-javac ./createTextFile.java
 echo "build java file"
+javac ./createTextFile.java
+echo "build java class"
+jar cf ./createTextFile.jar ./createTextFile.class
+echo "build java jar"
+
