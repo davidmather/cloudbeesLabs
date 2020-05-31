a1d7ba1d1be8e1e230355bde056742640c9b3d81
David <davidmather@live.ie>
2020-05-31 23:54:31 +0100

    t

--- jenkins/build.sh
+++ jenkins/build.sh
@@ -1,6 +1,6 @@
 echo "build java file"
 javac ./createTextFile.java
 echo "build java class"
-jar cf ./createTextFile.jar ./createTextFile.class
+jar cf ./target/createTextFile.jar ./createTextFile.class
 echo "build java jar"
 
